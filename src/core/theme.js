f7867b2e73c3a43f22e02ab499da0f4f3ca49b92
Marcin Izdebski <mizdebski123@gmail.com>
2023-03-22 19:31:32 +0100

    Added font and global styles

---
+++ src/core/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+    color: {
+        white:"#ffffff",
+          },
+    breakPoint: {
+        mobileMax:767,
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './core/App';
+import { GlobalStyle } from './core/GlobalStyle';
+import { theme } from './core/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );

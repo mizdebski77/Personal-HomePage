5e4915de77d011976781a3618620689807fd1516
Marcin Izdebski <mizdebski123@gmail.com>
2023-04-18 15:08:22 +0200

    Removed delay

--- src/common/sections/Portfolio/gitHubSaga.js
+++ src/common/sections/Portfolio/gitHubSaga.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest, delay} from "redux-saga/effects";
+import { call, put, takeLatest} from "redux-saga/effects";
 import { getRepos } from "./getApi";
 import { fetchRepos, fetchReposError, fetchReposSuccess } from "./githubSlice";
 
